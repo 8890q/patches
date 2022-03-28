b662fa47da93a6e4c0c3dc6d00dcdf26126bcae4
ivanmeler <i_ivan@windowslive.com>
2022-03-28 16:15:31 +0000

    Update patches list

--- picks.sh
+++ picks.sh
@@ -3,12 +3,6 @@
 # Abort early on error
 set -eE
 
-## Project: LineageOS/android_external_setupcompat
-
-./vendor/lineage/build/tools/repopick.py 326705 # Add option to look at main app for Partner resources
-./vendor/lineage/build/tools/repopick.py 326706 # Enable DayNight and dynamic colors by default
-./vendor/lineage/build/tools/repopick.py 326707 # Import pixel setupwizard overlay resources
-
 ## Project: LineageOS/android_frameworks_av
 
 ./vendor/lineage/build/tools/repopick.py 320337 # Request to reset effect buffer in clearInputBuffer
@@ -19,29 +13,18 @@ set -eE
 ./vendor/lineage/build/tools/repopick.py 318763 # SystemUI: runtime configurable audio panel location
 ./vendor/lineage/build/tools/repopick.py 320714 # SystemUI: add burnIn protection
 ./vendor/lineage/build/tools/repopick.py 320765 # SystemUI: Fix shutter sound
-./vendor/lineage/build/tools/repopick.py 321160 # Keyguard: Allow disabling fingerprint wake-and-unlock
 ./vendor/lineage/build/tools/repopick.py 321337 # Deprioritize important developer notifications
 ./vendor/lineage/build/tools/repopick.py 321338 # Allow disabling important developer notifications
 ./vendor/lineage/build/tools/repopick.py 321339 # Allow disabling USB notifications
-./vendor/lineage/build/tools/repopick.py 326692 # Skip screen on animation when wake and unlock via biometrics
-./vendor/lineage/build/tools/repopick.py 326735 # Revert "base: add Trust usb restrictor"
-./vendor/lineage/build/tools/repopick.py 326736 # Rewrite trust USB restriction handling
 ./vendor/lineage/build/tools/repopick.py 322825 # Add CHANNEL_MODE_DUAL_CHANNEL constant
 ./vendor/lineage/build/tools/repopick.py 322826 # Add Dual Channel into Bluetooth Audio Channel Mode developer options menu
 ./vendor/lineage/build/tools/repopick.py 322827 # Allow SBC as HD audio codec in Bluetooth device configuration
 ./vendor/lineage/build/tools/repopick.py 322828 # Explicitly make SBC Dual Channel an optional (HD) codec
-
-## Project: LineageOS/android_lineage-sdk
-
-./vendor/lineage/build/tools/repopick.py 325903 # lineage-sdk: Remove NativeHelper class
-./vendor/lineage/build/tools/repopick.py 326734 # Log failure to update DATABASE_VERSION
-./vendor/lineage/build/tools/repopick.py 326737 # Move trust usb setting to global, and drop keyguard from name
-./vendor/lineage/build/tools/repopick.py 326740 # Create LineageGlobalSettingListPreference
-./vendor/lineage/build/tools/repopick.py 326980 # Fix Usb Restrictor if USB HAL 1.3+ is not present
+./vendor/lineage/build/tools/repopick.py 326692 # Skip screen on animation when wake and unlock via biometrics
 
 ## Project: LineageOS/android_packages_apps_Bluetooth
 
-./vendor/lineage/build/tools/repopick.py 322838 # 322838: SBC Dual Channel (SBC HD Audio) support
+./vendor/lineage/build/tools/repopick.py 322838 # SBC Dual Channel (SBC HD Audio) support
 
 ## Project: LineageOS/android_packages_apps_CarrierConfig
 
@@ -58,15 +41,6 @@ set -eE
 ./vendor/lineage/build/tools/repopick.py 326383 # DeskClock: Remove legacy support
 ./vendor/lineage/build/tools/repopick.py 326994 # DeskClock: Remove compatibility to old APIs
 
-## Project: LineageOS/android_packages_apps_Dialer
-
-./vendor/lineage/build/tools/repopick.py 324509 # Dialer: remove unused drawables
-./vendor/lineage/build/tools/repopick.py 324510 # Dialer: Material You
-
-## Project: LineageOS/android_packages_apps_Eleven
-
-./vendor/lineage/build/tools/repopick.py 327078 # Move loader init after view setup
-
 ## Project: LineageOS/android_packages_apps_LineageParts
 
 ./vendor/lineage/build/tools/repopick.py 326739 # Trust: Add option to always disable USB gadgets
@@ -74,26 +48,15 @@ set -eE
 
 ## Project: LineageOS/android_packages_apps_Messaging
 
-./vendor/lineage/build/tools/repopick.py 324404 # Messaging: use Material You colors
-./vendor/lineage/build/tools/repopick.py 324504 # Messaging: remove unused drawables
-./vendor/lineage/build/tools/repopick.py 324505 # Messaging: Update checkmark PNG drawable with XML drawable
+./vendor/lineage/build/tools/repopick.py 324405 # Add support for text/vCard and text/vCalendar MIME type
 
 ## Project: LineageOS/android_packages_apps_Settings
 
-./vendor/lineage/build/tools/repopick.py 325369 # Settings: Fix the failing strings
-./vendor/lineage/build/tools/repopick.py 325897 # Settings: Support dark mode on 'Vision Settings'
-./vendor/lineage/build/tools/repopick.py 325928 # Settings: Apply SUW button theming to WiFi page
-./vendor/lineage/build/tools/repopick.py 327013 # Settings: Accessibility wants fullscreen, too!
-./vendor/lineage/build/tools/repopick.py 327067 # Settings: Use display version for, well, displayed version
 ./vendor/lineage/build/tools/repopick.py 322833 # Add Dual Channel into Bluetooth Audio Channel Mode developer options menu
 
 ## Project: LineageOS/android_packages_apps_SetupWizard
 
 ./vendor/lineage/build/tools/repopick.py 323145 # SUW: Enable status bar outside of FinishActivity
-./vendor/lineage/build/tools/repopick.py 325324 # SUW: Enable partner resources and switch to DayNight
-./vendor/lineage/build/tools/repopick.py 325325 # SUW: Set button themes
-./vendor/lineage/build/tools/repopick.py 325326 # SUW: Redesign Welcome page based on Pixel
-./vendor/lineage/build/tools/repopick.py 326875 # SUW: add page for navigation mode
 
 ## Project: LineageOS/android_packages_apps_Trebuchet
 
@@ -123,10 +86,6 @@ set -eE
 
 ./vendor/lineage/build/tools/repopick.py 326989 # NavigationBarNoHint: Set taskbar_frame_height to 0dp
 
-## Project: LineageOS/android_packages_services_Telephony
-
-./vendor/lineage/build/tools/repopick.py 317558 # Telephony: Add ERI configuration for U.S. Cellular
-
 ## Project: LineageOS/android_system_bt
 
 ./vendor/lineage/build/tools/repopick.py 319781 # Add option to change eSCO Transport Unit Size to 16
@@ -137,7 +96,6 @@ set -eE
 ## Project: LineageOS/android_system_core
 
 ./vendor/lineage/build/tools/repopick.py 317912 # init: Don't run update_sys_usb_config if /data isn't mounted
-./vendor/lineage/build/tools/repopick.py 325898 # fs_mgr: Don't run clean_scratch_files on non-dynamic devices
 
 ## Project: LineageOS/android_vendor_lineage
 
