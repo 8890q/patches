e96929311b5cb264660aa5a5c27664d79c35b812
ivanmeler <i_ivan@windowslive.com>
2022-11-19 14:49:28 +0000

    Regenerate patches needed for 20 on s7 based on https://github.com/TrebleDroid with some legacy patches leftover

--- apply.sh
+++ apply.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd frameworks/libs/net
-git am --signoff < ~/patches/frameworks_libs_net/0001-Add-support-for-non-bpf-usecase.patch
+git am --signoff < ~/patches/frameworks_libs_net/0001-Support-no-BPF-usecase.patch
 cd ../../../
 cd system/netd
-git am --signoff < ~/patches/system_netd/0001-Add-support-for-non-bpf-usecase.patch
+git am --signoff < ~/patches/system_netd/0001-Add-no-BPF-usecase-support.patch
 cd ../../
 cd packages/modules/Connectivity
-git am --signoff < ~/patches/packages_modules_Connectivity/0001-Add-support-for-non-bpf-usecase.patch
+git am --signoff < ~/patches/packages_modules_Connectivity/0001-Support-no-BPF-usecase.patch
 cd ../
 cd NetworkStack
 git am --signoff < ~/patches/packages_modules_NetworkStack/0001-Revert-Enable-parsing-netlink-events-from-kernel-sin.patch
@@ -16,7 +16,7 @@ cd adb
 git am --signoff < ~/patches/packages_modules_adb/0001-adb-Bring-back-support-for-legacy-FunctionFS.patch
 cd ../../../
 cd system/bpf
-git am --signoff < ~/patches/system_bpf/0001-Add-support-for-non-bpf-usecase.patch
+git am --signoff < ~/patches/system_bpf/0001-Support-no-BPF-usecase.patch
 cd ..
 cd security
 git am --signoff < ~/patches/system_security/0001-keystore-hackup.patch
@@ -25,6 +25,6 @@ cd frameworks/native
 git am --signoff < ~/patches/frameworks_native/0001-Disable-gpu-service.patch
 cd ../../
 cd system/core
-git am --signoff < ~/patches/system_core/0001-Revert-init-Treat-failure-to-create-a-process-group-.patch
+git am --signoff < ~/patches/system_core/0001-Add-no-BPF-usecase-support.patch
 cd ../../
 
