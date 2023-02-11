12432a51f9e75f03f04cbca3548dabbf36c6e7ae
ivanmeler <i_ivan@windowslive.com>
2023-02-11 08:54:02 +0000

    add revert  script

---
+++ revert.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd system/netd
+git reset --hard HEAD~1
+cd ../../
+cd hardware/libhardware
+echo "skipping hardware/libhardware"
+cd ../../
+cd packages/modules/adb
+git reset --hard HEAD~1
+cd ../../../
+cd system/bpf
+git reset --hard HEAD~1
+cd ../
+cd security
+echo "skipping system/security"
+cd ../../
+cd frameworks/base
+echo "skipping frameworks/base"
+cd ../../
+
