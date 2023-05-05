45cf14a9657be599af7bd466f98c532f432d4125
ivanmeler <i_ivan@windowslive.com>
2023-05-05 13:25:59 +0000

    add qcom revert script

---
+++ qcom_revert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd system/netd
+git reset --hard HEAD~1
+cd ../../
+cd packages/modules/adb
+git reset --hard HEAD~1
+cd ../../../
+cd system/bpf
+git reset --hard HEAD~1
+cd ../../
+cd frameworks/base
+git reset --hard HEAD~1
+cd ../../
+
